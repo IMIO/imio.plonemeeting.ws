894e3c06a7346b0d3efdb893bbed1a4544dcf3ec
Martin Peeters <martin.peeters@affinitic.be>
2015-06-01 11:32:22 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(
+    name='imio.ws.pm',
+    version=version,
+    description="",
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='https://github.com/IMIO/imio.ws.pm.git',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['imio', 'imio.ws'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone',
+        'plone.api',
+        'imio.amqp',
+        'imio.wsrequest.core',
+        'imio.wsresponse.core',
+    ],
+    extras_require={
+        'test': ['plone.app.testing'],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
